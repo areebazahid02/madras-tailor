d76acae7d6b833084cca7431cd0687a47eac7b01
Areeba Zahid <areebazahidk@gmail.com>
2026-07-10 01:02:44 +0530

    updated server of render

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const connectDB = require("./config/db");
 
 connectDB();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>{
     console.log(`SERVER RUNNING ON PORT ${PORT}`);
